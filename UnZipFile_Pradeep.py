ee126ae0629736a8e689d875ac435b9c90897653
pradeep.adiga <pradeep.adiga@gmail.com>
2016-03-24 22:45:20 +0530

    V2

--- UnZipFile_Pradeep.py
+++ UnZipFile_Pradeep.py
@@ -1,6 +1,6 @@
 import zipfile
 
-filetoextract = zipfile.ZipFile("Data\\Dummy\\Test.zip", 'r')
+filetoextract = zipfile.ZipFile("Data/Dummy/Test.zip", 'r')
 destdir = "Data/Dummy/Extract"
 filetoextract.extractall(destdir)
 filetoextract.close()
