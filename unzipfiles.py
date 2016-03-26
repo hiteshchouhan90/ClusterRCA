c777c32be0a7a144517bf82af7493ecb7d1f1ea9
Ravinder Pal Singh <ravinder.aoulakh@gmail.com>
2016-03-26 14:01:08 +0530

    Unzipping now working

--- unzipfiles.py
+++ unzipfiles.py
@@ -3,11 +3,10 @@
 # to add to the complexity pyunpack requires either cabextract or 7z to be installed
 # Hence I've installed 7z and added "C:\Program Files\7-Zip" to the environment variable
 
-from pyunpack import Archive
+import zipfile
 
-def unzipfile(filename,destfolder):
-
-    Archive(filename).extractall(destfolder)
+with zipfile.ZipFile('c:\\users\\ravsingh\\test.zip', "r") as z:
+    z.extractall("C:\\test\\")
 
 # Testing the unzip function
 # Giving the destination folder as TempFolder because at this point we don't know to which ServerName the zip file belongs to
