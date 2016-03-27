cca6335c98d85ebea577925b848406f410d2addb
Ravinder Pal Singh <ravinder.aoulakh@gmail.com>
2016-03-27 14:43:16 +0530

    Unzipping now working

--- unzipfiles.py
+++ unzipfiles.py
@@ -4,8 +4,15 @@
 # Hence I've installed 7z and added "C:\Program Files\7-Zip" to the environment variable
 
 import zipfile
+from datetime import datetime
 
-with zipfile.ZipFile('c:\\users\\ravsingh\\test.zip', "r") as z:
+# Get location of a SDP file
+sdplocation=input("Please provide zipped SDP file: ")
+
+new_path = '%s_%s' % (datetime.now().strftime(FORMAT), path)
+
+# Extract all the files from the compressed archive
+with zipfile.ZipFile(sdplocation, "r") as z:
     z.extractall("C:\\test\\")
 
 # Testing the unzip function
