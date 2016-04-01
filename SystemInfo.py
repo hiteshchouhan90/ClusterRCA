3bef7aa7c454c05bbb0dec06e6151a1405e682ca
pradeep.adiga <pradeep.adiga@gmail.com>
2016-04-01 23:31:45 +0530

    Modified SystemInfo.py to include the Processor's details. Earlier it was reporting only the number of processors

--- SystemInfo.py
+++ SystemInfo.py
@@ -12,15 +12,24 @@ import glob
 def GetSysInfo(rootdirectory, servernames,outputfile):
 
     sysinfolist = ["Host Name:", "OS Name:", "System Boot Time:", "System Manufacturer:", "System Model:",
-                   "System Type:", "Processor(s):", "BIOS Version:", "Time Zone:", "Total Physical Memory:",
+                   "System Type:",  "BIOS Version:", "Time Zone:", "Total Physical Memory:",
                    "Available Physical Memory:", "Virtual Memory: Max Size:", "Virtual Memory: Available:"]
 
     for servername in servernames:
         outputfile.write("~" * 20 + "\n" + "Details of: " + servername + "\n" + "~" * 20 + "\n")
         sysinfofile = glob.glob(rootdirectory + "/" + servername.upper() + "\*System_Information.txt")[0]
 
+        start = 0
         with open(sysinfofile, "r", encoding="utf-16") as file:
             for line in file:
+                # The following lines have been added because we need the processor details after the word "Processor(s)"
+                if "Processor(s)" in line:
+                    start = 1
+                if "BIOS Version:" in line:
+                    start = 0
+                if start == 1:
+                    outputfile.write(line)
+
                 for item in sysinfolist:
                     if item in line:
                         outputfile.write(line)
