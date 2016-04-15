fb45e2770976d2730ed22260a856521cb1ff3f7a
Ravinder Pal Singh <ravinder.aoulakh@gmail.com>
2016-04-15 07:48:08 +0530

    SP_Configure,  dm_os_cluster_nodes,  Loaded Modules DONE

--- SP_Configure.py
+++ SP_Configure.py
@@ -18,14 +18,31 @@ def GetSPConfigure(rootdirectory, servernames,outputfile):
 
         if len(glob.glob(rootdirectory + "/" + servername.upper() + "\*sp_sqldiag_Shutdown.OUT"))>0:
             sqldiag = glob.glob(rootdirectory + "/" + servername.upper() + "\*sp_sqldiag_Shutdown.OUT")[0]
-            outputfile.write("~" * 15 + "\n" + "SQL Server Configuration Details: " + servername + "\n" + "~" * 15 + "\n" * 2)
+            outputfile.write("~" * 30 + "\n" + "SQL Server Configuration Details: " + servername + "\n" + "~" * 30 + "\n" * 2)
 
             with open(sqldiag, "r") as diagfile:
                 for line in diagfile:
-                    if 'sp_configure' in line:
+                    if '-> sp_configure' in line:
                         next_n_lines = list(islice(diagfile, 71))
                         outputfile.writelines(next_n_lines)
                         break
+
+            outputfile.write("\n" *2+"~" * 30 + "\n" + "Loaded Modules: " + servername + "\n" + "~" * 30 + "\n" * 2)
+
+            with open(sqldiag, "r") as diagfile:
+                start=0
+                for line in diagfile:
+                    if '-> sys.dm_os_loaded_modules' in line:
+                        start=1
+                        for line2 in diagfile:
+                            if 'Microsoft' not in line2 and 'os_nodes'not in line2:
+                                outputfile.write(line2)
+                            elif '->' in line2:
+                                break
+                    elif start == 1:
+                        break
+
+
 
 
     print("Got SP_Configure output for " + servername.upper())
