47875037830916d1ddb9a3793df516e171f19e3c
pradeep.adiga <pradeep.adiga@gmail.com>
2016-04-23 00:32:02 +0530

    Testing GUI using tkinter in Testbed.py

--- TestBed.py
+++ TestBed.py
@@ -1,30 +1,70 @@
 import sys, glob, os
 from itertools import islice
 from operator import itemgetter
-
-with open("C:\Python35\Scripts\dist\SDP\COCTXSQLPRD01\CTXSQLPRD_MSSQLSERVER_1033_sp_sqldiag_Shutdown.OUT", "r") as diagfile:
-    start = 0
-    linecount=0
-    waitstatslist=[]
-    for line in diagfile:
-        if 'sys.dm_os_wait_stats' in line:
-            start =1
-            linecount=1
-#            next_n_lines = list(islice(diagfile, 71))
-        if 'sys.dm_os_waiting_tasks' in line:
-            break
+from tkinter import *
+from tkinter.filedialog import askopenfilename
+from tkinter.messagebox import showerror
+from datetime import date
 
 
-        if start==1:
-            if linecount>3:
-                if len(line)>3 and "FULLTEXT GATHERER" not in line:
-                    waitstatslist.append(line.split())
-            linecount+=1
+def load_file():
+    fname = askopenfilename(filetypes=[("Cabinet files", "*.cab")])
+    if fname:
+        try:
+            print("Got the file :" + fname)
+        except:  # <- naked except is a bad idea
+            showerror("Open Source File", "Failed to read file\n'%s'" % fname)
+        return
 
-    linecount=0
-    waitstatslist.sort(key= lambda waitstat: int(waitstat[2]), reverse=True)
-    for item in waitstatslist:
-        if linecount<6:
-            print(item[0] + "\t"*4 + item[2])
-        linecount+=1
+top = Tk()
 
+
+lblinput = Label(top, text="Enter file name here")
+lblyear = Label(top, text="Year:")
+lblMonth = Label(top, text="Month:")
+lblDate = Label(top, text="Date:")
+lblHour = Label(top, text="Hour:")
+
+entryinput = Entry(top)
+
+browsebutton = Button(text="Browse", command=load_file, width=10)
+
+# Creating date and time picker here
+yearval = StringVar(top)
+monthval = StringVar(top)
+dateval = StringVar(top)
+timeval = StringVar(top)
+
+# Getting the current year and -1
+yearlist = []
+yearlist.append(date.today().year)
+yearlist.append(date.today().year-1)
+
+yearbtn = OptionMenu(top, yearval, *yearlist)
+monthbtn = OptionMenu(top, monthval, *list(range(1,13)  ))
+datebtn = OptionMenu(top, dateval, *list(range(1,32)  ) )
+timebtn = OptionMenu(top, timeval, *list(range(1,25)  ) )
+# Aligning the user controls here
+inputframe = Frame(top)
+#inputframe.pack(fill=X, side=BOTTOM)
+inputframe.columnconfigure(0, weight=1)
+inputframe.columnconfigure(1, weight=1)
+inputframe.columnconfigure(2, weight=1)
+inputframe.columnconfigure(3, weight=1)
+inputframe.columnconfigure(4, weight=1)
+inputframe.columnconfigure(5, weight=1)
+inputframe.columnconfigure(6, weight=1)
+
+lblinput.grid(row=0, column=0)
+entryinput.grid(row=0, column=1)
+browsebutton.grid(row=0, column=2)
+lblyear.grid(row=3, column=0)
+yearbtn.grid(row=3, column=1)
+lblMonth.grid(row=3, column=2)
+monthbtn.grid(row=3, column=3)
+lblDate.grid(row=3, column=4)
+datebtn.grid(row=3, column=5)
+lblHour.grid(row=3, column=6)
+timebtn.grid(row=3, column=7)
+
+top.mainloop()
